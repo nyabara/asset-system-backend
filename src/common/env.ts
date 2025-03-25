1dedf0eefe88d7640bbda76a9f04754783c4bb94
nyabara <nyabarayoma1@gmail.com>
2025-03-25 13:14:28 +0300

    fixing azure

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
-import { cleanEnv, port, str, url } from "envalid";
+import { cleanEnv, port, str, url, bool } from "envalid";
 
 config();
 
@@ -9,11 +9,12 @@ export const env = cleanEnv(process.env, {
   POSTGRES_HOST: str(),
   POSTGRES_PORT: port({ default: 5432 }),
   POSTGRES_DB: str(),
+  POSTGRES_SSL: bool({ default: false }), // ✅ Use `bool()` to correctly validate boolean values
 
   PORT: port({ default: 5000 }),
 
   JWT_SECRET: str(),
   GOOGLE_CLIENT_SECRET: str(),
   GOOGLE_CLIENT_ID: str(),
-  FRONTEND_URL: url()
+  FRONTEND_URL: url(),
 });
