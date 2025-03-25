d1182648b27dddec8bfff06b9370aa63795cf073
nyabara <nyabarayoma1@gmail.com>
2025-03-25 18:20:19 +0300

    fixing azure

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,13 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// //import * as cookieParser from 'cookie-parser';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, { abortOnError: false });
-
-//     // Enable cookie parsing middleware
-//     //app.use(cookieParser());
-
-//   const config = new DocumentBuilder()
-//     .setTitle('API Documentation')
-//     .setDescription('The API description for Ene-System App')
-//     .setVersion('1.0')
-//     .addTag('cats')
-//     .build();
-
-//   const document = SwaggerModule.createDocument(app, config);
-//   SwaggerModule.setup('api', app, document);
-
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+//import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
 
-  // Enable CORS for Azure compatibility
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+    // Enable cookie parsing middleware
+    //app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
@@ -48,10 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  // Use Azure-assigned port
-  const PORT = process.env.PORT || 8080;
-  await app.listen(PORT);
-  console.log(` Server running on port ${PORT}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
