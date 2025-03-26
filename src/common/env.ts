6321c5065695ce54dcf47bf2fcee0b5d8eba1287
nyabara <nyabarayoma1@gmail.com>
2025-03-26 22:18:47 +0300

    fixing azure variables

--- src/common/env.ts
+++ src/common/env.ts
@@ -3,16 +3,31 @@ import { cleanEnv, port, str, url, bool } from "envalid";
 
 config();
 
+// export const env = cleanEnv(process.env, {
+//   POSTGRES_USER: str(),
+//   POSTGRES_PASSWORD: str(),
+//   POSTGRES_HOST: str(),
+//   POSTGRES_PORT: port({ default: 5432 }),
+//   POSTGRES_DB: str(),
+//   POSTGRES_SSL: str({ default: "false" }),
+//   PORT: port({ default: 5000 }),
+//   JWT_SECRET: str(),
+//   GOOGLE_CLIENT_SECRET: str(),
+//   GOOGLE_CLIENT_ID: str(),
+//   FRONTEND_URL: url(),
+// });
 export const env = cleanEnv(process.env, {
-  POSTGRES_USER: str(),
-  POSTGRES_PASSWORD: str(),
-  POSTGRES_HOST: str(),
-  POSTGRES_PORT: port({ default: 5432 }),
-  POSTGRES_DB: str(),
-  POSTGRES_SSL: str({ default: "false" }),
+  AZURE_POSTGRESQL_USER : str(),
+  AZURE_POSTGRESQL_PASSWORD : str(),
+  AZURE_POSTGRESQL_HOST: str(),
+  AZURE_POSTGRESQL_PORT: port({ default: 5432 }),
+  AZURE_POSTGRESQL_DATABASE : str(),
+  AZURE_POSTGRESQL_SSL: str({ default: "false" }),
   PORT: port({ default: 5000 }),
   JWT_SECRET: str(),
   GOOGLE_CLIENT_SECRET: str(),
   GOOGLE_CLIENT_ID: str(),
   FRONTEND_URL: url(),
 });
+
+
--- src/providers/database/pool.provider.ts
+++ src/providers/database/pool.provider.ts
@@ -7,11 +7,11 @@ export const poolProvider: Provider[] = [
     provide: 'DATABASE_POOL',
     useFactory: () => {
       return new Pool({
-        host: env.POSTGRES_HOST,
-        user: env.POSTGRES_USER,
-        password: env.POSTGRES_PASSWORD,
-        database: env.POSTGRES_PASSWORD,
-        port: env.POSTGRES_PORT,
+        host: env.AZURE_POSTGRESQL_HOST,
+        user: env.AZURE_POSTGRESQL_USER,
+        password: env.AZURE_POSTGRESQL_PASSWORD,
+        database: env.AZURE_POSTGRESQL_DATABASE,
+        port: env.AZURE_POSTGRESQL_PORT,
         idleTimeoutMillis: 30000,
       });
     },
