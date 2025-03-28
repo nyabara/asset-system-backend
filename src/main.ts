d2305913859d596a2f1747c037dfeb271967782d
nyabara <nyabarayoma1@gmail.com>
2025-03-28 12:06:24 +0300

    fixing azure port

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   //await app.listen(process.env.PORT ?? 3000);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  //await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 8080, '0.0.0.0');
+
 
 }
 
